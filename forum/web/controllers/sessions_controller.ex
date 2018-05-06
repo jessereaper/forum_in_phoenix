311ecd5c279efe20602a340f884abc1a27d835a6
Chad Ostrowski <hi@chadoh.com>
2018-05-06 11:21:05 -0400

    Fix indentation

--- forum/web/controllers/sessions_controller.ex
+++ forum/web/controllers/sessions_controller.ex
@@ -5,10 +5,8 @@ defmodule Rumbl.SessionController do
     render conn, "new.html"
   end
 
-  def create(conn, %{"session" => %{"username" => user, "password" =>
-                                     pass}}) do
-    case Rumbl.Auth.login_by_username_and_pass(conn, user, pass, repo:
-                                               Repo) do
+  def create(conn, %{"session" => %{"username" => user, "password" => pass}}) do
+    case Rumbl.Auth.login_by_username_and_pass(conn, user, pass, repo: Repo) do
       {:ok, conn} ->
         conn
         |> put_flash(:info, "Welcome back!")
